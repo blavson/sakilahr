37d98325ce79650380e8250336244ad10a825583
blavson <blava1979@gmail.com>
2022-12-17 13:59:39 +0400

    Add all blade and logic

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeesController extends Controller
 {
     public function all()
     {
-        
+        $employees = Employee::all()->take(50);
+        return view('employees.all', ['employees' => $employees ]);
     }
 }
---
+++ resources/views/employees/all.blade.php
@@ -0,0 +1,34 @@
+@extends('main')
+
+
+@section('body')
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 100%;">
+        <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+            <svg class="bi pe-none me-2" width="30" height="24">
+                <use xlink:href="#bootstrap"/>
+            </svg>
+            <span class="fs-5 fw-semibold">Employees</span>
+        </a>
+
+        <div class="list-group list-group-flush border-bottom scrollarea">
+            @foreach($employees as $employee)
+                <a href="#" class="list-group-item list-group-item-action  py-3 lh-sm" aria-current="true">
+                    <div class="d-flex w-100 align-items-center justify-content-between">
+                        <div class="col-sm-1">
+                            <strong>{{ $employee->employee_id }}</strong>
+                        </div>
+                        <div class="col-sm-11">
+                            <strong class="mb-1">{{$employee->last_name}}   {{ $employee->first_name }}</strong>
+                            <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and
+                                date.
+                            </div>
+
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="b-example-divider b-example-vr"></div>
+@endsection
