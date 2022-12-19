a206de1fe4cf59b6f704a48c0cb93736058bf4ef
blavson <blava1979@gmail.com>
2022-12-19 13:29:42 +0400

    Change blade files

--- resources/views/employees/all.blade.php
+++ resources/views/employees/all.blade.php
@@ -4,15 +4,12 @@
 @section('body')
     <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 100%;">
         <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-            <svg class="bi pe-none me-2" width="30" height="24">
-                <use xlink:href="#bootstrap"/>
-            </svg>
             <span class="fs-5 fw-semibold">Employees</span>
         </a>
 
         <div class="list-group list-group-flush border-bottom scrollarea">
             @foreach($employees as $employee)
-                <a href="#" class="list-group-item list-group-item-action  py-3 lh-sm" aria-current="true">
+                <a href="/employees/{{ $employee->employee_id }}" class="list-group-item list-group-item-action  py-3 lh-sm" aria-current="true">
                     <div class="d-flex w-100 align-items-center justify-content-between">
                         <div class="col-sm-1">
                             <strong>{{ $employee->employee_id }}</strong>
---
+++ resources/views/employees/show.blade.php
@@ -0,0 +1,8 @@
+@extends('main')
+
+
+@section('body')
+    <strong class="mt-3">
+        {{$employee->last_name}}
+    </strong>
+@endsection
